3dc4401eb08fc0faf5d3fca98570ebf52d895a78
SimoneDUrso <simone.durso98@gmail.com>
2024-06-24 15:01:01 +0200

    [UPD] Initialization of variables

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let firstName = prompt("Inserisci qui il tuo nome")
+
+let surname = prompt("Inserisci qui il tuo cognome")
+
+let favouriteColor = prompt("Inserisci qui il tuo colore preferito")
+
+
+
