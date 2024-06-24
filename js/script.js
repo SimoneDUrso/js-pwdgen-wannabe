ec63b908c6a8d5753822f9e6cce44963a5fc9f87
SimoneDUrso <simone.durso98@gmail.com>
2024-06-24 15:09:18 +0200

    [UPD] Created "document.getElementById" for the graphic result on the screen

--- js/script.js
+++ js/script.js
@@ -6,5 +6,7 @@ let favouriteColor = prompt("Inserisci qui il tuo colore preferito")
 
 let concatenazione = `La password da te generata è: ${firstName}${surname}${favouriteColor}23`
 
+document.getElementById('concatenazione').innerHTML = concatenazione
+
 
 
