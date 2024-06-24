13742d6056626564cd57712891a73f8e71ef8303
SimoneDUrso <simone.durso98@gmail.com>
2024-06-24 15:03:48 +0200

    [UPD] Created a new variable to concatenate the previous variables

--- js/script.js
+++ js/script.js
@@ -4,5 +4,7 @@ let surname = prompt("Inserisci qui il tuo cognome")
 
 let favouriteColor = prompt("Inserisci qui il tuo colore preferito")
 
+let concatenazione = `La password da te generata è: ${firstName}${surname}${favouriteColor}23`
+
 
 
